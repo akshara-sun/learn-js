e3f476cba17acb1feee1711039d30508318f36e6
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-12-10 14:18:26 -0800

    path-sum-II

---
+++ leetcode/algorithms/depth-first-search /medium/path-sum-II.js
@@ -0,0 +1,40 @@
+//113.
+/*
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+ var pathSum = function(root, targetSum) {
+    let resArr = [];
+    let pathArr = [];
+    function helper(root, targetSum, resArr, pathArr) {
+        if (!root) return;
+        if (!root.left && !root.right) {
+            if (targetSum === root.val) {
+                pathArr.push(root.val);
+                resArr.push([...pathArr]);
+                pathArr.pop();
+                return;
+            }
+        }
+        pathArr.push(root.val)
+        helper(root.left, targetSum - root.val, resArr, pathArr);
+        helper(root.right, targetSum - root.val, resArr, pathArr);
+        pathArr.pop()
+    }
+    helper(root, targetSum, resArr, pathArr)
+    return resArr;
+};
