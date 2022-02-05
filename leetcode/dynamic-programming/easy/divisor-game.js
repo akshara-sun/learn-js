b85eaa8782067805397d2c656fa92638a1b0e153
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2022-02-05 12:40:48 -0800

    divisor game

---
+++ leetcode/dynamic-programming/easy/divisor-game.js
@@ -0,0 +1,25 @@
+//1025.
+/*
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < n and n % x == 0.
+Replacing the number n on the chalkboard with n - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return true if and only if Alice wins the game, assuming both players play optimally.
+*/
+
+//Runtime = O(n)
+const divisorGame = (n) =>  {
+    if (n === 1) {
+        return false;
+    }
+    if (n > 1) {
+        if (n % 2 === 0) {
+            return true
+        }
+        return false
+    }
+};
--- leetcode/dynamic-programming/easy/fibonacci-num.js
+++ leetcode/dynamic-programming/easy/fibonacci-num.js
@@ -1,3 +1,4 @@
+//509.
 //The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. 
 
 const fib = (n) => {
